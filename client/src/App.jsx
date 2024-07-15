b15d388039b1efaf59c8c2d7f35746254a68b132
zoro <zoroxdp@gmail.com>
2024-07-15 20:32:04 +0530

    feat
    Created the PlayerOne Screen

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GameScreen from './pages/GameScreen'
+
+const App = () => {
+  return (
+    <div className="flex justify-center bg-gray-900 h-screen">
+      <GameScreen />
+    </div>
+  )
+}
+
+export default App
